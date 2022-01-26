b5149e13d19f007f98602d7d71279e8b5e23c63c
VaffanClash <luca.feller2005@gmail.com>
2022-01-26 11:28:55 +0100

    Inizial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f8e1229f-acd4-41b1-ad86-a766cff09071';
+export const MODAL_CLOSE_SEC = 2.5;
